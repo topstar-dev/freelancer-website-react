db972492414a4001deafd7f689960009c4cb5aa6
Krunal Bapodara <59815019+krunalbapodara@users.noreply.github.com>
2022-10-21 10:17:41 +0530

    password length check removed from sign in page

--- src/pages/auth/signin/SignIn.tsx
+++ src/pages/auth/signin/SignIn.tsx
@@ -31,7 +31,6 @@ const validationSchema = yup.object({
     .required("Email is required"),
   password: yup
     .string()
-    .min(8, "Password should be of minimum 8 characters length")
     .required("Password is required"),
 });
 
