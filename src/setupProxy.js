76a5e915ec8e5cbc07dd683da94913307a82a80c
Krunal Bapodara <59815019+krunalbapodara@users.noreply.github.com>
2022-10-28 14:05:15 +0530

    changed proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        process.env.REACT_APP_PROXY_URL,
+        '/apicall',
         createProxyMiddleware({
             target: process.env.REACT_APP_BASE_URL,
             secure: false,
