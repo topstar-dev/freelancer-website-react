8d4c2b1d8d8ff2d7cfe697ab69ab126fedb4c58b
Krunal Bapodara <59815019+krunalbapodara@users.noreply.github.com>
2022-11-20 20:16:35 +0530

    rounx error page modified

--- src/pages/404/ErrorPage.tsx
+++ src/pages/404/ErrorPage.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
+import './errorPage.css';
 
 export default function ErrorPage() {
     const { t } = useTranslation()
@@ -7,8 +8,9 @@ export default function ErrorPage() {
         document.title = t('title.error-page')
     })
     return (
-        <>
+        <div className='error-container'>
+            <img className='error-symbol' src="/images/rounx-symbol-black.png" height="100" />
             <h1>404 - Page not found</h1>
-        </>
+        </div>
     )
 }
