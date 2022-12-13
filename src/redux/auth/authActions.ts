e0f4bfb3a919bc372eb6a79cdf0643300b5d90f8
Krunal Bapodara <59815019+krunalbapodara@users.noreply.github.com>
2022-12-13 10:13:03 +0530

    set en default language

--- src/redux/auth/authActions.ts
+++ src/redux/auth/authActions.ts
@@ -57,7 +57,7 @@ export const signInUser = createAsyncThunk(
             if (response.success) {
                 const currentLang = `${localStorage.getItem('i18nextLng')}`;
                 if (currentLang !== response.data.language) {
-                    dispatch(changeLanguage(response.data.language))
+                    dispatch(changeLanguage(['en', 'zh-CN'].includes(response.data.language) || 'en'))
                 }
                 if (response.data?.avatar_url) {
                     dispatch(imageDownload({ functionType: 'USER_AVATAR', fileName: response.data.avatar_url }))
