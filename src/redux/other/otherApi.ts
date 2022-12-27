b1e2ff9e041f4f025da313fc8e019f8afa16790e
Krunal Bapodara <59815019+krunalbapodara@users.noreply.github.com>
2022-12-27 20:24:27 +0530

    user avatar updated

--- src/redux/other/otherApi.ts
+++ src/redux/other/otherApi.ts
@@ -2,10 +2,6 @@ import { fetchFileApi } from "../apiCall";
 import { ImageDownloadInterface } from "./otherActions";
 
 export const downloadImage = async (imageData: ImageDownloadInterface) => {
-    const requestOptions = {
-        headers: {
-            "function-type": imageData.functionType
-        }
-    };
-    return fetchFileApi(`/user/v1/image/${imageData.fileName}`, requestOptions);
+    const requestOptions = {};
+    return fetchFileApi(`/user/v1/image/${imageData.fileName}?function_type=${imageData.functionType}`, requestOptions);
 };
