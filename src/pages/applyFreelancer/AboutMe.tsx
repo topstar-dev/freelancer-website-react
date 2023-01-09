000bbdffbdc8c9cdfdc4fb15050239fd414a4d29
Krunal Bapodara <59815019+krunalbapodara@users.noreply.github.com>
2023-01-09 18:10:14 +0530

    about me spacing fix

--- src/pages/applyFreelancer/AboutMe.tsx
+++ src/pages/applyFreelancer/AboutMe.tsx
@@ -87,7 +87,7 @@ const AboutMe = (props: any) => {
                     {formik =>
                         <WithTranslateFormErrors {...formik}>
                             <Box className={`freelancer-body`}>
-                                <Box className="freelancer-card-spacing">
+                                <Box className="freelancer-card-spacing" style={{ paddingTop: 48 }}>
                                     <TextField
                                         multiline={true}
                                         rows={8}
