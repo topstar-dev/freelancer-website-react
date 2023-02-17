f55aaa4589a7d6240f472bb724635e561fb14394
Krunal Bapodara <59815019+krunalbapodara@users.noreply.github.com>
2023-02-17 10:33:36 +0530

    click to share link copy

--- src/pages/profile/sections/userInfo/EditUserInfo.tsx
+++ src/pages/profile/sections/userInfo/EditUserInfo.tsx
@@ -1,14 +1,24 @@
 import EditIcon from '@mui/icons-material/Edit';
 import ShareIcon from '@mui/icons-material/Share';
 import { Box } from '@mui/system';
+import { useSnackbar } from 'notistack';
+import { useTranslation } from 'react-i18next';
 
 const EditUserInfo = () => {
+    const { t } = useTranslation();
+    const { enqueueSnackbar } = useSnackbar();
+
     return (<Box className="user-info-edit">
         <EditIcon
             className="user-info-edit-icon"
             style={{ marginLeft: 'auto' }}
         />
-        <ShareIcon className="user-info-edit-icon" />
+        <ShareIcon
+            className="user-info-edit-icon"
+            onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                enqueueSnackbar(t('profile.share-link-copied-to-clipboard'))
+            }} />
     </Box>)
 }
 
