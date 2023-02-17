395b6938f29b570a240120e03850cf81aebaf5bd
Krunal Bapodara <59815019+krunalbapodara@users.noreply.github.com>
2023-02-17 10:46:43 +0530

    profile about added

--- src/pages/profile/FreelancerProfile.tsx
+++ src/pages/profile/FreelancerProfile.tsx
@@ -51,7 +51,7 @@ export default function FreelancerProfile({ profile }: any) {
             </div>
             <div className="left-profile-section">
                 <UserInfo {...profileUserInfo} currentProfile={currentProfile} />
-                {isAboutDisplay && <About about={`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`} currentProfile={currentProfile} />}
+                {isAboutDisplay && <About about={profile.about} currentProfile={currentProfile} />}
                 {isSkillDisplay && <Skills skills={profile.skills} currentProfile={currentProfile} />}
                 {isExperienceDisplay && <Experiences experiences={profile.experiences} currentProfile={currentProfile} />}
                 {isEducationDisplay && <Educations educations={profile.educations} currentProfile={currentProfile} />}
